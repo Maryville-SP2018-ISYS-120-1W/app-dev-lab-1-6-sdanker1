7499a18d64871f614785ab5d237a5e1951bfc731
sdanker1 <35539544+sdanker1@users.noreply.github.com>
2018-02-15 21:31:24 -0600

    Changes

--- FirstTimeDebugging/ViewController.swift
+++ FirstTimeDebugging/ViewController.swift
@@ -2,15 +2,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad( {
+    override func viewDidLoad() {
         super.viewDidLoad()
+        var names = ["Tammy","Cole"]
+        names.removeFirst()
+        names.removeFirst()
+        names.removeFirst()
+        navigationController?.title = "Debugging"
+        let x = 4
         // Do any additional setup after loading the view, typically from a nib.
-        var sample = "sample"
+        let sample = "sample"
         print(sample)
         
         if false {
             print("Will this line of code ever be reached?")
-            someMetho()
+            someMethod()
         }
         
         
